e909ed74e170ebb7aa8dd55f615e24ff74ecdbf8
Michael Danko <MichaelDanko@icloud.com>
2016-03-21 13:58:55 -0400

    Completed 8/40. Lessons: 1/7

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -6,7 +6,7 @@ describe("About Functions", function () {
             return a + b;
         }
 
-        expect(add(1, 2)).toBe(FILL_ME_IN);
+        expect(add(1, 2)).toBe(3);
     });
 
     it("should know internal variables override outer variables", function () {
@@ -21,9 +21,9 @@ describe("About Functions", function () {
             return message;
         }
 
-        expect(getMessage()).toBe(FILL_ME_IN);
-        expect(overrideMessage()).toBe(FILL_ME_IN);
-        expect(message).toBe(FILL_ME_IN);
+        expect(getMessage()).toBe("Outer");
+        expect(overrideMessage()).toBe("Inner");
+        expect(message).toBe("Outer");
     });
 
     it("should allow extra function arguments", function () {
@@ -32,13 +32,13 @@ describe("About Functions", function () {
             return firstArg;
         }
 
-        expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
-
+        expect(returnFirstArg("first", "second", "third")).toBe("first");
+        
         function returnSecondArg(firstArg, secondArg) {
             return secondArg;
         }
 
-        expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
+        expect(returnSecondArg("only give first arg")).toBe(undefined);
 
         function returnAllArgs() {
             var argsArray = [];
@@ -48,7 +48,7 @@ describe("About Functions", function () {
             return argsArray.join(",");
         }
 
-        expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
+        expect(returnAllArgs("first", "second", "third")).toBe("first,second,third");
     });
 
     it("should pass functions as values", function () {
